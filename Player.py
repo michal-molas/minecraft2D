8a934d450c649d92f4f173ca13144278084a2419
michal-molas <molas.mich@gmail.com>
2019-11-09 23:10:58 +0100

    czyszczenie starego kodu

--- Player.py
+++ Player.py
@@ -91,7 +91,7 @@ class Player:
                 self.is_falling = False
                 self.start_fall(terrain)
 
-    def dig(self, terrain, events):
+    def dig(self, terrain, events, eq):
         for event in events:
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 clicked_block = self.clicked_block(events)
@@ -99,21 +99,24 @@ class Player:
                     rel_blocks_x = clicked_block[0] - self.block_x
                     rel_blocks_y = clicked_block[1] - self.block_y
 
-                    if (rel_blocks_x == 0 and (rel_blocks_y == 1 or rel_blocks_y == -1)) \
-                            or (rel_blocks_y == 0 and (rel_blocks_x == 1 or rel_blocks_x == -1)) \
-                            or (rel_blocks_y == -1 and rel_blocks_x == -1
-                                and (terrain.terrain[clicked_block[1] + 1][clicked_block[0]].transparent
-                                     or terrain.terrain[clicked_block[1]][clicked_block[0] + 1].transparent)) \
-                            or (rel_blocks_y == -1 and rel_blocks_x == 1
-                                and (terrain.terrain[clicked_block[1] + 1][clicked_block[0]].transparent
-                                     or terrain.terrain[clicked_block[1]][clicked_block[0] - 1].transparent)) \
-                            or (rel_blocks_y == 1 and rel_blocks_x == -1
-                                and (terrain.terrain[clicked_block[1] - 1][clicked_block[0]].transparent
-                                     or terrain.terrain[clicked_block[1]][clicked_block[0] + 1].transparent)) \
-                            or (rel_blocks_y == 1 and rel_blocks_x == 1
-                                and (terrain.terrain[clicked_block[1] - 1][clicked_block[0]].transparent
-                                     or terrain.terrain[clicked_block[1]][clicked_block[0] - 1].transparent)):
-                        terrain.terrain[clicked_block[1]][clicked_block[0]].change_type("sky")
+                    if terrain.terrain[clicked_block[1]][clicked_block[0]].breakable:
+                        if (rel_blocks_x == 0 and (rel_blocks_y == 1 or rel_blocks_y == -1)) \
+                                or (rel_blocks_y == 0 and (rel_blocks_x == 1 or rel_blocks_x == -1)) \
+                                or (rel_blocks_y == -1 and rel_blocks_x == -1
+                                    and (terrain.terrain[clicked_block[1] + 1][clicked_block[0]].transparent
+                                         or terrain.terrain[clicked_block[1]][clicked_block[0] + 1].transparent)) \
+                                or (rel_blocks_y == -1 and rel_blocks_x == 1
+                                    and (terrain.terrain[clicked_block[1] + 1][clicked_block[0]].transparent
+                                         or terrain.terrain[clicked_block[1]][clicked_block[0] - 1].transparent)) \
+                                or (rel_blocks_y == 1 and rel_blocks_x == -1
+                                    and (terrain.terrain[clicked_block[1] - 1][clicked_block[0]].transparent
+                                         or terrain.terrain[clicked_block[1]][clicked_block[0] + 1].transparent)) \
+                                or (rel_blocks_y == 1 and rel_blocks_x == 1
+                                    and (terrain.terrain[clicked_block[1] - 1][clicked_block[0]].transparent
+                                         or terrain.terrain[clicked_block[1]][clicked_block[0] - 1].transparent)):
+                            if terrain.terrain[clicked_block[1]][clicked_block[0]].collectable:
+                                eq.set_slot(terrain.terrain[clicked_block[1]][clicked_block[0]].type)
+                            terrain.terrain[clicked_block[1]][clicked_block[0]].change_type("sky")
 
     def move(self, events, terrain, eq):
         for event in events:
@@ -163,174 +166,7 @@ class Player:
     def update(self, events, terrain, eq):
         self.move(events, terrain, eq)
         self.update_current_block()
-        self.dig(terrain, events)
+        self.dig(terrain, events, eq)
 
     def draw(self, window):
         window.blit(self.player_png, (self.POS_ON_SCREEN_X, self.POS_ON_SCREEN_Y))
-
-    '''
-    def dig_down(self, terrain, x, y, eq):
-        if self.right_wall and terrain.terrain[y + 1][x - 1].breakable:
-            if terrain.terrain[y + 1][x].type == "tree":
-                self.cut_tree(terrain, x, y + 1, eq)
-            if terrain.terrain[y + 1][x].collectable:
-                eq.set_slot(terrain.terrain[y + 1][x - 1].type)
-            terrain.terrain[y + 1][x - 1].change_type("sky")
-        elif terrain.terrain[y + 1][x].breakable:
-            if terrain.terrain[y + 1][x].type == "tree":
-                self.cut_tree(terrain, x, y + 1, eq)
-            if terrain.terrain[y + 1][x].collectable:
-                eq.set_slot(terrain.terrain[y + 1][x].type)
-            terrain.terrain[y + 1][x].change_type("sky")
-
-    def move_left(self, terrain, x, y):
-        if not self.left_wall:
-            if terrain.terrain[y][x].transparent or self.right_wall:
-                self.position[0] -= 1
-            self.can_dig_left = False
-            self.can_dig_right = False
-        elif terrain.terrain[y][x - 1].breakable:
-            self.can_dig_left = True
-
-    def move_right(self, terrain, x, y):
-        if not self.right_wall:
-            if terrain.terrain[y][x].transparent:
-                self.position[0] += 1
-            self.can_dig_left = False
-            self.can_dig_right = False
-        elif terrain.terrain[y][x].breakable:
-            self.can_dig_right = True
-
-    def check_walls(self, terrain, x, y):
-        if self.position[0] % 32 == 16:
-            if not terrain.terrain[y][x - 1].transparent:
-                self.left_wall = True
-            else:
-                self.left_wall = False
-            if not terrain.terrain[y][x].transparent:
-                self.right_wall = True
-            else:
-                self.right_wall = False
-        else:
-            self.left_wall = False
-            self.right_wall = False
-
-    def dig(self, terrain, x, y, eq, events):
-        for event in events:
-            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                mouse_pos = pygame.mouse.get_pos()
-                dif_x = mouse_pos[0] - 32 * config.screen_width // 64 - 16
-                player_right_dist = 32 * config.screen_width // 64 - 1 - self.position[0] % 32 - 16 - 32 * config.screen_width // 64 - 16
-                dif_y = 32 * config.screen_height // 64 + 16 - mouse_pos[1]
-                print(player_right_dist)
-                if player_right_dist < dif_x < player_right_dist - 32 and -16 < dif_y < 16:
-                    terrain.terrain[y][x - 1].change_type("sky")
-                elif 32 * config.screen_width // 64 + 32 < mouse_pos[0] < 32 * config.screen_width // 64 + 64 \
-                        and 32 * config.screen_height // 64 < mouse_pos[1] < 32 * config.screen_height // 64 + 32:
-                    # dig right
-                    pass
-                elif 32 * config.screen_width // 64 < mouse_pos[0] < 32 * config.screen_width // 64 + 32\
-                        and 32 * config.screen_height // 64 - 32 < mouse_pos[1] < 32 * config.screen_height // 64:
-                    # dig up
-                    pass
-                elif 32 * config.screen_width // 64 < mouse_pos[0] < 32 * config.screen_width // 64 + 32\
-                        and 32 * config.screen_height // 64 + 32 < mouse_pos[1] < 32 * config.screen_height // 64 + 64:
-                    # dig down
-                    pass
-
-    def dig_left_right(self, terrain, x, y, eq):
-        if self.can_dig_left:
-            if terrain.terrain[y][x - 1].type == "tree":
-                self.cut_tree(terrain, x - 1, y, eq)
-            if terrain.terrain[y][x - 1].collectable:
-                eq.set_slot(terrain.terrain[y][x - 1].type)
-            terrain.terrain[y][x - 1].change_type("sky")
-            self.can_dig_left = False
-        if self.can_dig_right:
-            if terrain.terrain[y][x].type == "tree":
-                self.cut_tree(terrain, x, y, eq)
-            if terrain.terrain[y][x].collectable:
-                eq.set_slot(terrain.terrain[y][x].type)
-            terrain.terrain[y][x].change_type("sky")
-            self.can_dig_right = False
-
-    def dig_up_or_start_jump(self, terrain, x, y, eq):
-        if (terrain.terrain[y - 1][x].transparent or (terrain.terrain[y - 1][x - 1].transparent and self.right_wall)) \
-                and not (self.right_wall and not terrain.terrain[y - 1][x - 1].transparent
-                         and terrain.terrain[y - 1][x].transparent):
-            self.can_jump = False
-        elif terrain.terrain[y - 1][x].breakable:
-            if not self.right_wall:
-                if terrain.terrain[y - 1][x].type == "tree":
-                    self.cut_tree(terrain, x, y - 1, eq)
-                if terrain.terrain[y - 1][x].collectable:
-                    eq.set_slot(terrain.terrain[y - 1][x].type)
-                terrain.terrain[y - 1][x].change_type("sky")
-            else:
-                if terrain.terrain[y - 1][x - 1].type == "tree":
-                    self.cut_tree(terrain, x - 1, y - 1, eq)
-                if terrain.terrain[y - 1][x - 1].collectable:
-                    eq.set_slot(terrain.terrain[y - 1][x - 1].type)
-                terrain.terrain[y - 1][x - 1].change_type("sky")
-
-    @staticmethod
-    def cut_tree(terrain, x, y, eq):
-        i = 1
-        while terrain.terrain[y - i][x].type == "tree":
-            eq.set_slot(terrain.terrain[y - i][x].type)
-            terrain.terrain[y - i][x].change_type("sky")
-            i += 1
-        for n in range(3):
-            for m in range(3):
-                terrain.terrain[y - i - n][x - 1 + m].change_type("sky")
-
-    def move(self, terrain, eq, ev):
-
-        keys = pygame.key.get_pressed()
-        mouse = pygame.mouse.get_pressed()
-
-        player_index_y = self.position[1] // 32
-        if self.position[0] % 32 < 16:
-            player_index_x = self.position[0] // 32 + config.screen_width // 64
-        else:
-            player_index_x = self.position[0] // 32 + config.screen_width // 64 + 1
-
-        self.check_walls(terrain, player_index_x, player_index_y)
-
-        self.dig(terrain, player_index_x, player_index_y, eq, ev)
-
-        if keys[pygame.K_a]:
-            self.move_left(terrain, player_index_x, player_index_y)
-
-        if keys[pygame.K_d]:
-            self.move_right(terrain, player_index_x, player_index_y)
-
-        if mouse[0]:
-            self.dig_left_right(terrain, player_index_x, player_index_y, eq)
-
-        if self.can_jump:
-            self.start_fall(terrain, player_index_x, player_index_y)
-            if keys[pygame.K_w] and not self.is_falling:
-                self.dig_up_or_start_jump(terrain, player_index_x, player_index_y, eq)
-            if keys[pygame.K_s] and not self.already_digged and not self.is_falling:
-                self.dig_down(terrain, player_index_x, player_index_y, eq)
-                self.already_digged = True
-        else:
-            self.jump()
-
-        if not keys[pygame.K_s]:
-            self.already_digged = False
-
-        if keys[pygame.K_e]:
-            if not self.e_pressed:
-                if not eq.eq_opened:
-                    eq.eq_opened = True
-                else:
-                    eq.eq_opened = False
-            self.e_pressed = True
-
-        if not keys[pygame.K_e]:
-            self.e_pressed = False
-
-        self.fall(terrain, player_index_x, player_index_y)
-    '''
